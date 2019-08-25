871073d4041fb679fffe69ae00d3115c977f7285
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-08-25 22:04:49 +0200

    MOAR CHIMKENS 🐓

--- src/components/farm/Farm.tsx
+++ src/components/farm/Farm.tsx
@@ -2,15 +2,16 @@ import React, { useRef, useEffect } from "react";
 import styles from "./farm.module.scss";
 import henHouse from "../../sprites/hen-house.png";
 import waterHole from "../../sprites/water.png";
-import brownChicken1 from "../../sprites/brown-chicken-1.png";
-import brownChicken2 from "../../sprites/brown-chicken-2.png";
-import brownChicken3 from "../../sprites/brown-chicken-3.png";
 import { useWindowDimensions } from "../../utils/useWindowDimensions";
-import { drawImage, drawChicken } from "../../utils/drawImages";
+import { drawImage, drawAllChickens } from "../../utils/drawImages";
+
+const RESIZE_BY = 2;
 
 export const Farm: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const { width, height } = useWindowDimensions();
+  const resizedWidth = width / RESIZE_BY;
+  const resizedHeight = height / RESIZE_BY;
 
   useEffect(() => {
     if(!canvasRef.current) {
@@ -21,22 +22,20 @@ export const Farm: React.FC = () => {
       return;
     }
 
-    drawImage({ ctx, width, height, src: henHouse, top: 20, left: 20 });
-    drawImage({ ctx, width, height, src: waterHole, top: 20, left: 30 });
+    drawImage({ ctx, src: henHouse, top: 100, left: 100 });
+    drawImage({ ctx, src: waterHole, top: 120, left: 170 });
 
-    drawChicken({
-      ctx,
-      width,
-      height,
-      srcs: [brownChicken1, brownChicken2, brownChicken3],
-      top: 40,
-      left: 20
-    });
-  }, [canvasRef, width, height])
+    drawAllChickens(ctx, resizedWidth, resizedHeight);
+  }, [canvasRef, resizedWidth, resizedHeight])
 
   return (
     <div className={styles.wrapper}>
-      <canvas ref={canvasRef} width={width} height={height}></canvas>
+      <canvas
+        ref={canvasRef}
+        width={resizedWidth}
+        height={resizedHeight}
+        className={styles.canvas}
+      ></canvas>
     </div>
   );
 }
--- src/utils/drawImages.ts
+++ src/utils/drawImages.ts
@@ -1,54 +1,64 @@
 import { loadMultipleImages, loadImage } from "./loadImages";
+import brownChicken1 from "../sprites/brown-chicken-1.png";
+import brownChicken2 from "../sprites/brown-chicken-2.png";
+import brownChicken3 from "../sprites/brown-chicken-3.png";
+import orangeChicken1 from "../sprites/orange-chicken-1.png";
+import orangeChicken2 from "../sprites/orange-chicken-2.png";
+import orangeChicken3 from "../sprites/orange-chicken-3.png";
+import yellowChicken1 from "../sprites/yellow-chicken-1.png";
+import yellowChicken2 from "../sprites/yellow-chicken-2.png";
+import yellowChicken3 from "../sprites/yellow-chicken-3.png";
 
-interface ImageCoordinates {
+const MOVEMENT_PX = 2;
+
+interface DrawImageOptionsCommon {
+  ctx: CanvasRenderingContext2D;
   top: number;
   left: number;
-}
-
-interface DrawImageOptionsCommon extends ImageCoordinates {
-  ctx: CanvasRenderingContext2D;
-  width: number;
-  height: number;
 }
 
 interface DrawImageOptions extends DrawImageOptionsCommon {
   src: string;
 }
 
-interface DrawImagesOptions extends DrawImageOptionsCommon {
+interface DrawImagesOptions {
   srcs: string[];
+  width: number;
+  height: number;
+  ctx: CanvasRenderingContext2D
 }
 
-type GetPosFromWindowOptions = Omit<DrawImageOptionsCommon, "ctx">;
-type DrawFromCoordinates = DrawImageOptionsCommon & { img: HTMLImageElement };
+interface DrawFromCoordinatesOptions extends DrawImageOptionsCommon {
+  img: HTMLImageElement;
+}
 
-const getPosFromWindow = ({ width, height, top, left }: GetPosFromWindowOptions) => {
-  return {
-    dy: Math.floor(height * (top / 100)),
-    dx: Math.floor(width * (left / 100)),
-  }
+interface GetUpdatedPosOptions {
+  top: number;
+  left: number;
+  maxWidth: number;
+  maxHeight: number;
+  imgWidth: number;
+  imgHeight: number;
 }
 
 const drawFromCoordinates = ({
   top,
   left,
-  width,
-  height,
   ctx,
   img,
-}: DrawFromCoordinates) => {
-  const { dx, dy } = getPosFromWindow({ width, height, top, left });
-  ctx.clearRect(dx, dy, img.naturalWidth, img.naturalHeight);
+}: DrawFromCoordinatesOptions) => {
+  const imgWidth = img.naturalWidth;
+  const imgHeight = img.naturalHeight;
   ctx.drawImage(
     img,
     0,
     0,
     img.naturalWidth,
     img.naturalHeight,
-    dx,
-    dy,
-    img.naturalWidth,
-    img.naturalHeight
+    left,
+    top,
+    imgWidth,
+    imgHeight
   );
 }
 
@@ -57,17 +67,56 @@ export const drawImage = ({ src, ...props }: DrawImageOptions) => {
     if(!img) {
       return;
     }
+    props.ctx.clearRect( props.left, props.top, img.naturalWidth, img.naturalHeight);
     drawFromCoordinates({ ...props, img });
   })
 }
 
-export const drawChicken = ({
+const getUpdatedPos = ({
+  imgWidth,
+  imgHeight,
+  top,
+  left,
+  maxWidth,
+  maxHeight,
+}: GetUpdatedPosOptions) => {
+  const rand = Math.round(Math.random() * 3);
+  let posTop = top;
+  let posLeft = left;
+  
+  switch(rand) {
+    case 0:
+      posTop = Math.max(top - MOVEMENT_PX, 0);
+      break;
+    case 1:
+      posLeft = Math.min(left + MOVEMENT_PX, maxWidth - imgWidth);
+      break;
+    case 2:
+      posTop = Math.min(top + MOVEMENT_PX, maxHeight - imgHeight);
+      break;
+    case 3:
+      posLeft = Math.max(left - MOVEMENT_PX, 0);
+      break;
+    default:
+      console.error("Update position random number out of bounds");
+      break
+  }
+
+  return { posTop, posLeft };
+}
+
+const drawChicken = ({
   srcs,
-  ...props
+  width,
+  height,
+  ctx,
 }: DrawImagesOptions) => {
   loadMultipleImages(srcs).then(imgs => {
     let i = 0;
     let frameCount = 0;
+    let top = Math.round(Math.random() * (height - imgs[0].naturalHeight));
+    let left = Math.round(Math.random() * (width - imgs[0].naturalWidth));
+
     const walk = () => {
       frameCount++;
       if(frameCount < 15) {
@@ -77,11 +126,59 @@ export const drawChicken = ({
       frameCount = 0;
       const img = imgs[i];
 
-      drawFromCoordinates({ ...props, img });
+      ctx.clearRect(
+        left,
+        top,
+        (img.naturalWidth) + MOVEMENT_PX,
+        (img.naturalHeight) + MOVEMENT_PX,
+      );
+
+      const res = getUpdatedPos({
+        top,
+        left,
+        maxWidth: width,
+        maxHeight: height,
+        imgHeight: img.naturalHeight,
+        imgWidth: img.naturalWidth
+      });
+      top = res.posTop;
+      left = res.posLeft;
+
+      drawFromCoordinates({ top, left, img, ctx });
       i = i + 1 >= imgs.length - 1 ? 0 : i + 1;
 
       window.requestAnimationFrame(walk);
     }
     walk();
   })
+}
+
+export const drawAllChickens = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
+  const rand1 = Math.ceil(Math.random() * 15);
+  const rand2 = Math.ceil(Math.random() * 15);
+  const rand3 = Math.ceil(Math.random() * 15);
+  for(let i = 0; i < rand1; i++) {
+    drawChicken({
+      ctx,
+      width,
+      height,
+      srcs: [brownChicken1, brownChicken2, brownChicken3],
+    });
+  }
+  for(let i = 0; i < rand2; i++) {
+    drawChicken({
+      ctx,
+      width,
+      height,
+      srcs: [orangeChicken1, orangeChicken2, orangeChicken3],
+    });
+  }
+  for(let i = 0; i < rand3; i++) {
+    drawChicken({
+      ctx,
+      width,
+      height,
+      srcs: [yellowChicken1, yellowChicken2, yellowChicken3],
+    });
+  }
 }
