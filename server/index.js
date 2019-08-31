ab60bcce89e6a8f28a442433b54f95da1b5387d1
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-08-31 17:12:45 +0200

    Improvements server side

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 // Serving client
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
 	return res.sendFile(path.join(__dirname, '..', 'build', "index.html"));
 });
 
