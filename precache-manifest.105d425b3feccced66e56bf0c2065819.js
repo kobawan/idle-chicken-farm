bb0a49008e67d8fa21727b310217254ba46a5972
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-10-06 19:44:33 +0200

    Updates

---
+++ precache-manifest.105d425b3feccced66e56bf0c2065819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab44b9690ae38a62d3d073b56c8c3e52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04dd5688d49a4505e0d6",
+    "url": "/static/css/main.199e2d7f.chunk.css"
+  },
+  {
+    "revision": "ae94477b0b0dc39766a1",
+    "url": "/static/js/2.88de0774.chunk.js"
+  },
+  {
+    "revision": "04dd5688d49a4505e0d6",
+    "url": "/static/js/main.68ffebda.chunk.js"
+  },
+  {
+    "revision": "f5ccfe9929baf7e0cf58",
+    "url": "/static/js/runtime~main.7328c33a.js"
+  },
+  {
+    "revision": "507ab01a3c09d519b5020019ef3636a6",
+    "url": "/static/media/farmBg.507ab01a.png"
+  }
+]);
